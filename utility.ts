a52b22beca4d5ab231b60aaf1e1bed5906a8f305
M-Mafeni <mafeni99@gmail.com>
2022-12-04 21:13:36 +0000

    Add utility function for reading lines

--- utility.ts
+++ utility.ts
@@ -3,3 +3,7 @@ const fs = require("fs").promises;
 export async function readContentsFromFile(filePath: string): Promise<string> {
     return fs.readFile(filePath).then((res: any) => String(res));
 }
+
+export async function readLinesFromFile(filePath: string): Promise<string[]> {
+    return readContentsFromFile(filePath).then(text => text.split("\r\n"));
+} 
