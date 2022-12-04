6422ff7155d16e458d54606b6a764cc775ff0a5b
M-Mafeni <mafeni99@gmail.com>
2022-12-04 22:41:58 +0000

    Add day 4 part 2

--- 04/solution.ts
+++ 04/solution.ts
@@ -8,6 +8,12 @@ function doesPair1FullyContainPair2(p1: Pair, p2: Pair): boolean {
     return (x2 >= x1 && y2 <= y1);
 }
 
+function doesPair1OverlapPair2(p1: Pair, p2: Pair): boolean {
+    const [x1, y1] = p1;
+    const [x2, _y2] = p2;
+    return (x2 >= x1 && x2 <= y1);
+}
+
 function toPairs(line: string): [Pair, Pair] {
     const toPair = (val: string): Pair => {
         const [a, b] = val.split("-").map(Number)
@@ -25,6 +31,13 @@ async function solve() {
         return doesPair1FullyContainPair2(p1, p2) || doesPair1FullyContainPair2(p2, p1);
     }).length
     console.log(total1);
+
+    const total2 = pairList.filter((pairs) => {
+        const [p1, p2] = pairs;
+        return doesPair1OverlapPair2(p1, p2) || doesPair1OverlapPair2(p2, p1);
+    }).length
+    console.log(total2);
+
 }
 
 solve().catch((err) => {
