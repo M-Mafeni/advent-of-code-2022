7eed166c5e4a4de4a130724954008a9aa3e1409b
M-Mafeni <mafeni99@gmail.com>
2022-12-15 23:42:22 +0000

    Add Solution to Day 9 part 1

---
+++ 09/solution.ts
@@ -0,0 +1,114 @@
+import { readLinesFromFile } from "../utility";
+
+type Direction = "U" | "D" | "L" | "R";
+
+interface Move {
+    direction: Direction;
+    value: number;
+}
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+
+function updateTail(head: Position, tail: Position): Position {
+    const distance = {
+        dx: head.x - tail.x,
+        dy: head.y - tail.y
+    }
+    const newTail = {...tail};
+    if (distance.dx === Math.sign(distance.dx) && distance.dy === Math.sign(distance.dy)) {
+        // console.log({head, newTail, distance})
+        return newTail;
+    }
+
+    if (distance.dx === 2 && distance.dy === 0) {
+        // Head to the right, move tail to the right
+        newTail.x += 1;
+    } else if (distance.dx === -2 && distance.dy === 0) {
+        // Head to the left of tail
+        newTail.x -= 1;
+    } else if (distance.dy === 2 && distance.dx === 0) {
+        // Head above tail
+        newTail.y += 1;
+    } else if (distance.dy === -2 && distance.dx === 0) {
+        // Head below tail
+        newTail.y -= 1;
+    } else if (distance.dx !== 0 && distance.dy !== 0) {
+        // Not in same col or row, move tail diagonally
+        newTail.x += Math.sign(distance.dx);
+        newTail.y += Math.sign(distance.dy);
+    }
+
+    // console.log({head, newTail, distance})
+    return newTail;
+}
+
+function addPosToList(positions: Position[], newPos: Position) {
+    const exists = positions.some((p) => p.x === newPos.x && p.y === newPos.y);
+    if (!exists) {
+        positions.push(newPos);
+    }
+}
+
+// Returns the new position of head and tail
+function makeMove(head: Position, tail: Position, move: Move): [Position, Position, Position[]] {
+    const newHead = {...head};
+    let newTail = tail;
+    const positions: Position[] = [];
+    for (let i = 0; i < move.value; i++) {
+        switch (move.direction) {
+            case "U":
+                newHead.y += 1;
+                break;
+            case "D":
+                newHead.y -= 1;
+                break;
+            case "L":
+                newHead.x -= 1;
+                break;
+            case "R": 
+                newHead.x += 1;
+                break;
+        }
+        // updateTail
+        newTail = updateTail(newHead, newTail);
+        addPosToList(positions, newTail);
+    }
+    
+    return [newHead, newTail, positions];
+}
+
+// returns list of positions the tail has been in
+function runMoves(moves: Move[]): Position[] {
+    let head = {x: 0, y: 0};
+    let tail = {x: 0, y: 0};
+    const tailPositions = [{x: 0, y: 0}];
+
+    for (const move of moves) {
+        const [newHead, newTail, partialTailPos] = makeMove(head, tail, move);
+        // console.log({partialTailPos});
+        // console.log();
+        head = newHead;
+        tail = newTail;
+        partialTailPos.forEach(pos => addPosToList(tailPositions, pos)); 
+    }
+
+    return tailPositions;
+}
+
+async function solve() {
+    const lines = await readLinesFromFile(__dirname + "/input.txt");
+    const moves = lines.map((line) => {
+        const [direction, val1] = line.split(" ");
+        return {direction: direction as Direction, value: Number(val1)}
+    });
+
+    const tailPositions = runMoves(moves);
+    // console.log({tailPositions});
+    console.log(tailPositions.length);
+}
+
+solve().catch((err) => console.log("An error occurred", err));
