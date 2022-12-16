17a3c101c2f7a1eacd65b18cf9cfec65b35b7158
M-Mafeni <mafeni99@gmail.com>
2022-12-16 23:57:45 +0000

    Add script for creating folder structure

---
+++ createFolderForDay.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const day = process.argv[2];
+
+fs.mkdirSync(day);
+
+const tsTemplate = `
+async function solve() {
+
+}
+
+solve().catch(err => console.log("An error occurred", err));
+`
+fs.writeFileSync(`${day}/solution.ts`, tsTemplate);
+fs.writeFileSync(`${day}/input.txt`, "");
+fs.writeFileSync(`${day}/test.txt`, "");
